64062b823fb7a23699f3f775d476792100c6697e
yfjiang <1012501615@qq.com>
2021-02-20 10:01:23 +0800

    fix time computing bugs

--- benchmark_time.py
+++ benchmark_time.py
@@ -14,7 +14,6 @@ MODEL_LIST = {
 
 HARDWARE_LIST = ['CPU', 'GPU']
 
-
 WARM_UP = 10
 NUM_TEST = 20
 BATCH_SIZE = 16
@@ -43,19 +42,20 @@ this function is used for benchmarking the inference time of model in cpu (or di
 def inference_cpu():
     benchmark = {}
     print("------ CPU BENCH ------")
-    print("benchmarking model inference time begin...")
+    print("Begin benchmarking the time of model inference ...")
     with torch.no_grad():
         for model_name, model in MODEL_LIST.items():
+            print("now model is:", model_name)
             durations = []
             model.eval()
             for step, img in enumerate(img_loader):
                 start_time = time.time()
-                model.forward(img)
+                model(img)
                 end_time = time.time()
                 if step >= WARM_UP:
                     durations.append((end_time - start_time) * 1000)
             benchmark[model_name] = durations
-    print("benchmarking end")
+    print("------ END ------")
     return benchmark
 
 """
@@ -64,7 +64,7 @@ this function is used for benchmarking the inference time of model in gpu
 def inference_gpu():
     benchmark = {}
     print("------ GPU BENCH ------")
-    print("benchmarking model inference time begin...")
+    print("Begin benchmarking the time of model inference ...")
     with torch.no_grad():
         for model_name, model in MODEL_LIST.items():
             durations = []
@@ -74,19 +74,19 @@ def inference_gpu():
                 img = img.to('cuda')
                 torch.cuda.synchronize()
                 start_time = time.time()
-                model.forward(img)
+                model(img)
                 torch.cuda.synchronize()
                 end_time = time.time()
                 if step >= WARM_UP:
                     durations.append((end_time - start_time) * 1000)
             benchmark[model_name] = durations
-    print("benchmarking end")
+    print("------ END ------")
     return benchmark
 
 if __name__ == "__main__":
     inference_benchmark_cpu = pandas.DataFrame(inference_cpu())
     inference_benchmark_cpu.to_csv("results/model_inference_benchmark_cpu", index = False)
 
-    inference_benchmark_gpu = pandas.DataFrame(inference_gpu())
-    inference_benchmark_gpu.to_csv("results/model_inference_benchmark_gpu", index = False)
+    #inference_benchmark_gpu = pandas.DataFrame(inference_gpu())
+    #inference_benchmark_gpu.to_csv("results/model_inference_benchmark_gpu", index = False)
 
